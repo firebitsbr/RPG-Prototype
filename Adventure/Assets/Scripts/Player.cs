c119d9d6f17cb4923b4d335424e190a6222908d7
Dreaddisk <joey_manuel@outlook.com>
2018-09-12 14:55:11 -0400

    Initial Commit
    First commit for the prototype

---
+++ Adventure/Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	#region Variables
+	[Header("Visuals")]
+	public GameObject model;
+	public float rotatingSpeed = 2f;
+	[Header("Movement")]
+	public float movingVelocity;
+	public float jumpingVelocity = 5f;
+
+	[Header("Equipments")]
+	public Sword sword;
+	RaycastHit hit;
+
+	private bool canJump;
+	private Rigidbody playerRigidBody;
+	private Quaternion targetModelRotation;
+	#endregion Variables
+
+	#region Functions
+	private void Start()
+	{
+		playerRigidBody = GetComponent<Rigidbody>();
+		targetModelRotation = Quaternion.Euler(0,0,0);
+	}
+	private void FixedUpdate()
+	{
+		model.transform.rotation = Quaternion.Lerp(model.transform.rotation,
+									  targetModelRotation, Time.deltaTime * rotatingSpeed);
+		ProcessInput();
+
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.01f))
+		{
+			Debug.Log("Is not jumping");
+			canJump = true;
+		}
+	}
+
+	#endregion Functions
+
+	#region Methods
+
+	public void ProcessInput()
+	{
+		GetComponent<Rigidbody>().velocity = new Vector3(
+			0,
+			playerRigidBody.velocity.y,
+			0
+			);
+		if (Input.GetKey(KeyCode.D)| Input.GetKey(KeyCode.RightArrow))
+		{
+			playerRigidBody.velocity = new Vector3(movingVelocity,
+										playerRigidBody.velocity.y,
+										playerRigidBody.velocity.z);
+//			transform.position += Vector3.right * movingSpeed * Time.deltaTime;
+			targetModelRotation = Quaternion.Euler(0,270,0);
+		}
+		if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow))
+		{
+			playerRigidBody.velocity = new Vector3( -movingVelocity,
+										 playerRigidBody.velocity.y,
+										 playerRigidBody.velocity.z);
+//			transform.position += Vector3.left * movingSpeed * Time.deltaTime;
+			targetModelRotation = Quaternion.Euler(0,90,0);
+//			model.transform.localEulerAngles = new Vector3(0,90, 0);
+		}
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+		{
+			playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x,
+													playerRigidBody.velocity.y,
+													movingVelocity);
+//			transform.position += Vector3.forward * movingSpeed * Time.deltaTime;
+			targetModelRotation = Quaternion.Euler(0,180,0);
+//			model.transform.localEulerAngles = new Vector3(0,180,0);
+		}
+		if (Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.DownArrow))
+		{
+			playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x,
+												  playerRigidBody.velocity.y,
+												 -movingVelocity);
+
+//			transform.position += Vector3.back * movingSpeed * Time.deltaTime;
+			targetModelRotation = Quaternion.Euler(0,0,0);
+//			model.transform.localEulerAngles = new Vector3(0,0,0);
+		}
+
+		if (canJump && Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("Is jumping");
+			canJump = false;
+			playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x,
+				jumpingVelocity,
+				playerRigidBody.velocity.z);
+		}
+
+		// Check equipment interaction
+		if(Input.GetKeyDown("z"))
+		{
+			sword.Attack();
+		}
+	}
+	#endregion Methods
+
+} // Main Class
---
+++ Adventure/Assets/Scripts/Sword.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour 
+{
+    #region Varuables
+    public float swingingSpeed = 2f;
+    public float cooldownSpeed = 2f;
+    public float attackDuration = 0.35f;
+    public float cooldownDuration = 0.5f;
+
+    public float cooldownTimer;
+    private Quaternion targetRotation;
+
+    private bool isAttacking;
+    #endregion
+    private void Start()
+    {
+        targetRotation = Quaternion.Euler(0,0,0);
+    }
+
+    private void Update()
+    {
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * swingingSpeed);
+
+        cooldownTimer -= Time.deltaTime;
+    }
+
+    public void Attack()
+    {
+        if(cooldownTimer > 0f)
+        {
+            return;
+        }
+        targetRotation = Quaternion.Euler(-90, 0, 0);
+
+        cooldownTimer = cooldownDuration;
+
+        StartCoroutine(CooldownWait());
+    }
+
+    private IEnumerator CooldownWait()
+    {
+        yield return new WaitForSeconds(attackDuration);
+
+        targetRotation = Quaternion.Euler(0,0,0);
+    }
+}
